76c9e87d04c7a378560281a8a2420824b2725262
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-03-27 15:33:30 +0200

    Simplify

--- src/main.rs
+++ src/main.rs
@@ -42,35 +42,33 @@ fn main() -> ! {
     let mut peripherals = stm32::Peripherals::take().unwrap();
     let cp = Peripherals::take().unwrap();
 
-    cortex_m::interrupt::free(|cs| {
-        let mut rcc = peripherals
-            .RCC
-            .configure()
-            .sysclk(8.mhz())
-            .freeze(&mut peripherals.FLASH);
+    let mut rcc = peripherals
+        .RCC
+        .configure()
+        .sysclk(8.mhz())
+        .freeze(&mut peripherals.FLASH);
 
-        let gpioc = peripherals.GPIOC.split(&mut rcc);
+    let gpioc = peripherals.GPIOC.split(&mut rcc);
 
-        let mut delay = Delay::new(cp.SYST, &rcc);
+    let mut delay = Delay::new(cp.SYST, &rcc);
 
-        let mut orange = gpioc.pc8.into_push_pull_output(cs);
-        let mut green = gpioc.pc9.into_push_pull_output(cs);
-        let mut red = gpioc.pc6.into_push_pull_output(cs);
-        let mut blue = gpioc.pc7.into_push_pull_output(cs);
+    let mut orange = cortex_m::interrupt::free(|cs| gpioc.pc8.into_push_pull_output(cs));
+    let mut green = cortex_m::interrupt::free(|cs| gpioc.pc9.into_push_pull_output(cs));
+    let mut red = cortex_m::interrupt::free(|cs| gpioc.pc6.into_push_pull_output(cs));
+    let mut blue = cortex_m::interrupt::free(|cs| gpioc.pc7.into_push_pull_output(cs));
 
-        let mut leds: [Led; 4] = [
-            Led::new(&mut orange),
-            Led::new(&mut green),
-            Led::new(&mut red),
-            Led::new(&mut blue),
-        ];
+    let mut leds: [Led; 4] = [
+        Led::new(&mut orange),
+        Led::new(&mut green),
+        Led::new(&mut red),
+        Led::new(&mut blue),
+    ];
 
-        let mut i = 0;
+    let mut i = 0;
 
-        loop {
-            leds[i % 4].toggle().unwrap();
-            i += 1;
-            delay.delay_ms(20_u16);
-        }
-    })
+    loop {
+        leds[i % 4].toggle().unwrap();
+        i += 1;
+        delay.delay_ms(20_u16);
+    }
 }
