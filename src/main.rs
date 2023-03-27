d7e74c3c0584c7a094bcccc24a0303e815fc6d51
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2023-03-27 15:31:33 +0200

    Simplify

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 #![no_std]
 #![no_main]
 
-extern crate panic_halt;
-
 use core::convert::Infallible;
+use core::panic::PanicInfo;
+use core::sync::atomic::{self, Ordering};
 
 use cortex_m::peripheral::Peripherals;
 use cortex_m_rt::entry;
@@ -13,16 +13,26 @@ use stm32f0xx_hal::prelude::*;
 use stm32f0xx_hal::stm32;
 
 struct Led<'a> {
-    pin: &'a mut dyn ToggleableOutputPin<Error = Infallible>,
+    pin: &'a mut dyn ToggleableOutputPin<Error=Infallible>,
 }
 
 impl Led<'_> {
-    fn new(pin: &mut dyn ToggleableOutputPin<Error = Infallible>) -> Led {
+    fn new(pin: &mut dyn ToggleableOutputPin<Error=Infallible>) -> Led {
         Led { pin }
     }
 
     fn toggle(&mut self) -> Result<(), Infallible> {
         self.pin.toggle()
+    }
+}
+
+#[inline(never)]
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {
+        // compiler_fence does not emit any machine code, but restricts the
+        // kinds of memory re-ordering the compiler is allowed to do.
+        atomic::compiler_fence(Ordering::SeqCst);
     }
 }
 
@@ -43,15 +53,10 @@ fn main() -> ! {
 
         let mut delay = Delay::new(cp.SYST, &rcc);
 
-        let orange = gpioc.pc8.into_push_pull_output(cs);
-        let green = gpioc.pc9.into_push_pull_output(cs);
-        let red = gpioc.pc6.into_push_pull_output(cs);
-        let blue = gpioc.pc7.into_push_pull_output(cs);
-
-        let mut orange = orange.downgrade();
-        let mut green = green.downgrade();
-        let mut red = red.downgrade();
-        let mut blue = blue.downgrade();
+        let mut orange = gpioc.pc8.into_push_pull_output(cs);
+        let mut green = gpioc.pc9.into_push_pull_output(cs);
+        let mut red = gpioc.pc6.into_push_pull_output(cs);
+        let mut blue = gpioc.pc7.into_push_pull_output(cs);
 
         let mut leds: [Led; 4] = [
             Led::new(&mut orange),
